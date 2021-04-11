49990b72240fdd1808e2085b49f943eb11d096a5
Raghad Al Samrout <raghadsamrout@ieee.org>
2021-04-11 11:10:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+function countDown() {
+  let i=0;
+  while(i<11)
+  {
+    console.log(i);
+    i++;
+  }
+}
+//array=["Ada", "Brendan", "Ali"];
+
+function writeCards(array, eventname){
+let messages=[];
+  for(let i=0;i<array.length;i++)
+  {
+    messages.push(`Thank you, ${array[i]}, for the wonderful ${eventname} gift!`);
+
+  }
+  return messages;
+}
+console.log(writeCards(["Ada", "Brendan", "Ali"], "birthday"));
