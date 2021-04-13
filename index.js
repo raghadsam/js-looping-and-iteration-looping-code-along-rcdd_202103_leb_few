2e90c245bc701cfcf3f8b33af485b12102c11425
Raghad Al Samrout <raghadsamrout@ieee.org>
2021-04-13 08:58:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ function countDown() {
 }
 //array=["Ada", "Brendan", "Ali"];
 
+<<<<<<< HEAD
 function writeCards(array, eventname){
 let messages=[];
   for(let i=0;i<array.length;i++)
@@ -19,3 +20,13 @@ let messages=[];
   return messages;
 }
 console.log(writeCards(["Ada", "Brendan", "Ali"], "birthday"));
+=======
+function writeCards(array[],eventname){
+array=["Ada", "Brendan", "Ali"];
+eventname="birthday";
+  for(let i=0;i<array.length;i++)
+  {
+    console.log("Thank you, ${array[i]} for the wonderful ${eventname} gift!")
+  }
+}
+>>>>>>> c94836243c2f340911e6fb98722010b4a8cc6590
